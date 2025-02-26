d5ca93aa21b8420c3b2c06a1a91d59e1f11007c2
omar marrakchi <marrakchi.omar@esprit.tn>
2025-02-26 22:43:59 +0100

    ajout bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ResidencesComponentComponent } from './residences-component/residences-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResidencesComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/residences-component/residences-component.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { Residence } from 'src/core/models/residence';
+
+@Component({
+  selector: 'app-residences-component',
+  templateUrl: './residences-component.component.html',
+  styleUrls: ['./residences-component.component.css']
+})
+export class ResidencesComponentComponent {
+  listResidences:Residence[]=[
+     {id:1,"name": "El fel","address":"Borj Cedria", "image":"../../assets/images/R1.png", status: "Disponible"},
+     {id:2,"name": "El yasmine", "address":"Ezzahra","image":"../../assets/images/R2.jpg", status: "Disponible" },
+     {id:3,"name": "El Arij", "address":"Rades","image":"../../assets/images/R3.jpg", status: "Vendu"},
+     {id:4,"name": "El Anber","address":"inconnu", "image":"../../assets/images/R4.jpg", status: "En Construction"}
+   ];
+
+   favoriteResidences: Residence[] = [];
+
+   showLocation(address: string): void {
+    if(address === 'inconnu') {
+      alert('L’adresse de cette résidence est « inconnu »');
+    } else {
+      alert(`Adresse: ${address}`);
+    }
+   }
+
+   addToFavorites(residence: Residence): void {
+    if (!this.favoriteResidences.includes(residence)) {
+      this.favoriteResidences.push(residence);
+      alert(`${residence.name} a été ajouté aux favoris.`);
+    } else {
+      alert(`${residence.name} est déjà dans les favoris.`);
+    }
+  }
+ 
+
+}
