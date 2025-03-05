ec299daf431126ce8a41dd8d87081bf3c5d5beec
omar marrakchi <marrakchi.omar@esprit.tn>
2025-03-05 15:45:17 +0100

    ajout appartement

--- src/app/Apartments/apartments-by-residence/apartments-by-residence.component.ts
+++ src/app/Apartments/apartments-by-residence/apartments-by-residence.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Apartment } from 'src/core/models/apartment';
 
 @Component({
   selector: 'app-apartments-by-residence',
   templateUrl: './apartments-by-residence.component.html',
   styleUrls: ['./apartments-by-residence.component.css']
 })
-export class ApartmentsByResidenceComponent {
+export class ApartmentsByResidenceComponent implements OnInit {
+
+ listApartments: Apartment[] = [
+     { apartNum: 101, floorNum: 1, surface: 75, terrace: true, surfaceterrace: 10, category: 'T2', ResidenceId: 1, image: "../../../assets/images/app1.jpg" },
+     { apartNum: 102, floorNum: 1, surface: 80, terrace: false, surfaceterrace: 0, category: 'T3', ResidenceId: 1, image: "../../../assets/images/app2.jpg" },
+     { apartNum: 201, floorNum: 2, surface: 90, terrace: true, surfaceterrace: 15, category: 'T3', ResidenceId: 2, image: "../../../assets/images/app3.jpg" },
+     { apartNum: 202, floorNum: 2, surface: 85, terrace: false, surfaceterrace: 0, category: 'T2', ResidenceId: 2, image: "../../../assets/images/app4.jpg" },
+     { apartNum: 301, floorNum: 3, surface: 100, terrace: true, surfaceterrace: 20, category: 'T4', ResidenceId: 3, image: "../../../assets/images/app5.jpg" },
+     { apartNum: 302, floorNum: 3, surface: 95, terrace: false, surfaceterrace: 0, category: 'T3', ResidenceId: 3, image: "../../../assets/images/app6.jpg" },
+     { apartNum: 401, floorNum: 4, surface: 110, terrace: true, surfaceterrace: 25, category: 'T4', ResidenceId: 4, image: "../../../assets/images/app7.jpg" },
+     { apartNum: 402, floorNum: 4, surface: 105, terrace: false, surfaceterrace: 0, category: 'T3', ResidenceId: 4, image: "../../../assets/images/app8.jpg" }
+   ];
+
+   residenceId!: number;
+   appartement!: Apartment[];
+
+
+
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.residenceId = +params.get('id')!;
+      this.loadlisteResidance();
+    });
+  }
+
+
+  loadlisteResidance() {
+    this.appartement = this.listApartments.filter(apartment => apartment.ResidenceId === this.residenceId);
+  }
+
+  
 
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,9 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { HomeComponent } from './home/home.component';
 import { ResidenceDetailsComponent } from './Residences/residence-details/residence-details.component';
 import { AddResidenceComponent } from './Residences/add-residence/add-residence.component';
+import { ApartmentsComponent } from './Apartments/apartments/apartments.component';
+import { ApartmentsByResidenceComponent } from './Apartments/apartments-by-residence/apartments-by-residence.component';
+import { AddApartmentComponent } from './Apartments/add-apartment/add-apartment.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
@@ -13,6 +16,9 @@ const routes: Routes = [
   {path: 'residence/:id', component: ResidenceDetailsComponent},
   {path: 'add-residence/:id', component: AddResidenceComponent},
   {path: 'add-residence', component: AddResidenceComponent},
+  {path: 'apartment', component: ApartmentsComponent},
+  {path: 'apartments-list/:id', component: ApartmentsByResidenceComponent},
+  {path: 'add-appartement', component: AddApartmentComponent},
   {path: '**', component: NotFoundComponent}
 ];
 
